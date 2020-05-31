0450d726b9879ceeb5746d131ce20e94e4915c68
datdawt <66020680+datdawt@users.noreply.github.com>
2020-05-31 13:15:49 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ var _0xBitcoinToken = artifacts.require("./_0xBitcoinToken.sol");
 var _0xTestToken = artifacts.require("./_0xTestToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(_0xBitcoinToken);
-  deployer.deploy(_0xTestToken);
+  deployer.deploy(_dadatoken);
+  deployer.deploy(_dadatoken);
 };
